10f2f3ad30d5dce15f4319336e6a42ad89ef3b89
chandrabhansinghsarngdevot960-afk <chandrabhansinghsarngdevot960@gmail.com>
2026-04-18 09:32:48 +0530

    Update data.js

--- data.js
+++ data.js
@@ -3,7 +3,7 @@ const myNews = [
     { title: "ACCOUNTS (CENTER / EXAMINER BILL ETC.)", link: "#" },
     { title: "SCRUTINY-2026 Apply Online Now", link: "#" },
     { title: "Provisional Ans.Key for Objection of M.G. Sarvoday Exam", link: "#" },
-    { title: "Download New Syllabus 2026 PDF", link: "https://example.com/syllabus.pdf" }
+    { title: "Download New Syllabus 2026 PDF", link: "#" }
 ];
 
 const myVideos = [
@@ -12,7 +12,6 @@ const myVideos = [
 ];
 
 const photos = {
-    // Ye links 100% chalenge, baad mein aap apni files se replace kar sakte hain
     cm: "https://well-pink-hiryfqelqh.edgeone.app/how-bhajan-lal-sharmas-rpsc-appointments-signal-a-clean-break-250035975-16x9_0.jpg", 
     minister: "https://picsum.photos/seed/minister/200/250",
     building: "https://picsum.photos/seed/board/800/400"

d2f8bd2590de47bba212c175e4e3043c5ea26cbf
chandrabhansinghsarngdevot960-afk <chandrabhansinghsarngdevot960@gmail.com>
2026-04-18 09:22:26 +0530

    21

--- scripts.js
+++ scripts.js
@@ -1,23 +1,20 @@
 document.addEventListener('DOMContentLoaded', () => {
-    // Load News
-    const newsList = document.getElementById('news-list');
-    newsList.innerHTML = myNews.map(item => `
-        <a href="${item.link}" class="news-item" target="_blank">
-            <i class="fas fa-chevron-right" style="font-size: 10px;"></i> ${item.title}
-        </a>
+    // News Render
+    const newsBox = document.getElementById('news-list');
+    newsBox.innerHTML = myNews.map(n => `
+        <a href="${n.link}" class="news-link" target="_blank">🔹 ${n.title}</a>
     `).join('');
 
-    // Load Videos
-    const videoGrid = document.getElementById('video-grid');
-    videoGrid.innerHTML = myVideos.map(vid => `
-        <div class="vid-card">
-            <iframe src="https://www.youtube.com/embed/${vid.id}" frameborder="0" allowfullscreen></iframe>
-            <p style="font-size:12px; text-align:center;">${vid.title}</p>
+    // Video Render
+    const videoBox = document.getElementById('video-grid');
+    videoBox.innerHTML = myVideos.map(v => `
+        <div class="video-item">
+            <iframe src="https://www.youtube.com/embed/${v.id}" frameborder="0" allowfullscreen></iframe>
+            <p>${v.title}</p>
         </div>
     `).join('');
 
-    // Load Photos
+    // Photos Load
     document.getElementById('cm-photo').src = photos.cm;
     document.getElementById('edu-photo').src = photos.minister;
-    document.querySelector('.main-img').src = photos.building;
 });

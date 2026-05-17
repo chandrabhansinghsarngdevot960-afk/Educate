06d808836aa33a25f4903daa4e6e4a571d416470
chandrabhansinghsarngdevot960-afk <chandrabhansinghsarngdevot960@gmail.com>
2026-05-17 11:46:18 +0530

    1

---
+++ events.js
@@ -0,0 +1,4 @@
+const eventsData = [
+ {name:"BGMI Tournament",time:"2026-05-20T18:00:00"},
+ {name:"Movie Night",time:"2026-05-22T21:00:00"}
+];
---
+++ giveaway.js
@@ -0,0 +1,7 @@
+const giveawayData = [
+ {
+  name:"Nitro Giveaway",
+  end:"2026-05-25T20:00:00",
+  participants:["user1","user2","user3"]
+ }
+];
